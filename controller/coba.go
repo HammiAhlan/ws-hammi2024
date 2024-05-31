3c7c65625c7c1c92b29ff2a9e3d99087b3a84554
HammiAhlan <hammiahlan@gmail.com>
2024-05-31 16:01:19 +0700

    PRESENSI

--- controller/coba.go
+++ controller/coba.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"github.com/HammiAhlan/ws-hammi2024/config"
 	"github.com/aiteung/musik"
-	cek "github.com/indrariksa/cobapakcage/module"
+	cek "github.com/HammiAhlan/Backend_Package/module"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	inimodel "github.com/HammiAhlan/Backend_Package/model"
+	
 	"net/http"
 )
 
@@ -52,4 +54,33 @@ func GetPresensiID(c *fiber.Ctx) error {
 		})
 	}
 	return c.JSON(ps)
+}
+
+func InsertDataPresensi(c *fiber.Ctx) error {
+	db := config.Ulbimongoconn
+	var presensi inimodel.Presensi
+	if err := c.BodyParser(&presensi); err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"status":  http.StatusInternalServerError,
+			"message": err.Error(),
+		})
+	}
+	insertedID, err := cek.InsertPresensi(db, "presensi",
+		presensi.Longitude,
+		presensi.Latitude,
+		presensi.Location,
+		presensi.Phone_number,
+		presensi.Checkin,
+		presensi.Biodata)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"status":  http.StatusInternalServerError,
+			"message": err.Error(),
+		})
+	}
+	return c.Status(http.StatusOK).JSON(fiber.Map{
+		"status":      http.StatusOK,
+		"message":     "Data berhasil disimpan.",
+		"inserted_id": insertedID,
+	})
 }
--- url/url.go
+++ url/url.go
@@ -16,8 +16,11 @@ func Web(page *fiber.App) {
 	page.Patch("/", controller.Sink)
 	page.Delete("/", controller.Sink)
 	page.Options("/", controller.Sink)
+	
 
 	page.Get("/checkip", controller.Homepage) //ujicoba panggil package musik
 	page.Get("/presensi", controller.GetPresensi)
 	page.Get("/presensi/:id", controller.GetPresensiID) //menampilkan data presensi berdasarkan id
+	page.Post("/insert", controller.InsertDataPresensi)
+	
 }
